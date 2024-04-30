ba91508e5a1c27c1ee27c8a739952ec69b53afda
Smirnov Nikita <smirnovn@mtsbank.ru>
2024-04-30 20:16:48 +0300

    ES: get index by id

--- backend/proto-proxy/go/GetMovieByID.pb.validate.go
+++ backend/proto-proxy/go/GetMovieByID.pb.validate.go
@@ -161,12 +161,24 @@ func (m *GetMovieByIDResponse) validate(all bool) error {
 
 	var errors []error
 
+	// no validation rules for XIndex
+
+	// no validation rules for XId
+
+	// no validation rules for XVersion
+
+	// no validation rules for XSeqNo
+
+	// no validation rules for XPrimaryTerm
+
+	// no validation rules for Found
+
 	if all {
-		switch v := interface{}(m.GetMovie()).(type) {
+		switch v := interface{}(m.GetXSource()).(type) {
 		case interface{ ValidateAll() error }:
 			if err := v.ValidateAll(); err != nil {
 				errors = append(errors, GetMovieByIDResponseValidationError{
-					field:  "Movie",
+					field:  "XSource",
 					reason: "embedded message failed validation",
 					cause:  err,
 				})
@@ -174,16 +186,16 @@ func (m *GetMovieByIDResponse) validate(all bool) error {
 		case interface{ Validate() error }:
 			if err := v.Validate(); err != nil {
 				errors = append(errors, GetMovieByIDResponseValidationError{
-					field:  "Movie",
+					field:  "XSource",
 					reason: "embedded message failed validation",
 					cause:  err,
 				})
 			}
 		}
-	} else if v, ok := interface{}(m.GetMovie()).(interface{ Validate() error }); ok {
+	} else if v, ok := interface{}(m.GetXSource()).(interface{ Validate() error }); ok {
 		if err := v.Validate(); err != nil {
 			return GetMovieByIDResponseValidationError{
-				field:  "Movie",
+				field:  "XSource",
 				reason: "embedded message failed validation",
 				cause:  err,
 			}
